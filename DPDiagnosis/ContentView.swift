8265f16792c122a96ffa0ed11c962304d5fe9e93
松田尚也 <minorim_n@M-Damatsumatsuda.local>
2021-11-24 15:43:24 +0900

    add: base app files

---
+++ DPDiagnosis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DPDiagnosis
+//
+//  Created by 松田尚也 on 2021/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DPDiagnosis/DPDiagnosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DPDiagnosisApp.swift
+//  DPDiagnosis
+//
+//  Created by 松田尚也 on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DPDiagnosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
